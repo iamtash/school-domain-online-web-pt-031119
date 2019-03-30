8cbcac129f023dc22ee93491499f45b420e29681
Natasha Chernis <natashachernis@gmail.com>
2019-03-30 19:42:40 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -5,4 +5,12 @@ class School
     @school = school_name
     @roster = {}
   end
+
+  def add_student(name, grade)
+    if @roster[grade]
+      @roster[grade] << name 
+    else
+      @roster[grade] = []
+      @roster[grade] << name
+  end
 end
